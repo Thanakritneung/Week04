0f36302b7b04eaef3d6f44023c5b76760be5e257
58030171 <58030171@kmitl.ac.th>
2017-10-24 23:09:23 +0700

    read 10

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-
 namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            char ch;
-            Console.Write("Press a key followed by ENTER:");
-            ch = (char)Console.Read();  // get a char
-            Console.WriteLine("Your key is: " + ch);
-
+            string str;
+            Console.Write("Enter some characters.");
+            str = Console.ReadLine();
+            Console.WriteLine("You entered: " + str);
         }
     }
 }
