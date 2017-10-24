99cd7c275bcd2797f4c48f34496ffc498db34b8e
58030171 <58030171@kmitl.ac.th>
2017-10-24 21:58:01 +0700

    test {}

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,7 +6,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" {0} and {1}",3,6);
+            Console.WriteLine(" {0} and {2} {3}",3,6,4);
         }
     }
 }
